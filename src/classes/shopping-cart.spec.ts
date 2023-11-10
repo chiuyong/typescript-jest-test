3f24c1ab6b5a8f119ab40d8c3d53b838906e65ec
Chiu Yong <chiu@vert-capital.com>
2023-11-10 16:07:26 -0300

    test: shopping-cart

---
+++ src/classes/shopping-cart.spec.ts
@@ -0,0 +1,70 @@
+import { ShoppingCart } from './shopping-cart';
+import { Discount } from './discount';
+import { CartItem } from './interfaces/cart-item';
+
+const createSut = () => {
+  const discountMock = createDiscountMock();
+  const sut = new ShoppingCart(discountMock);
+  return { sut, discountMock };
+};
+
+const createDiscountMock = () => {
+  class DiscountMock extends Discount {}
+  return new DiscountMock();
+};
+
+const createCartItem = (name: string, price: number) => {
+  class CartItemMock implements CartItem {
+    constructor(public name: string, public price: number) {}
+  }
+  return new CartItemMock(name, price);
+};
+
+const createSutWithProducts = () => {
+  const { sut, discountMock } = createSut();
+  const cartItem1 = createCartItem('Camiseta', 40);
+  const cartItem2 = createCartItem('Caneta', 1);
+  sut.addItem(cartItem1);
+  sut.addItem(cartItem2);
+  return { sut, discountMock };
+};
+
+afterEach(() => jest.clearAllMocks());
+
+describe('ShoppingCart', () => {
+  it('should have be an empty cart when no product is added', () => {
+    // System under test
+    const { sut } = createSut();
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should have 2 cart items', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+  });
+
+  it('should test total and totalWithDiscount', () => {
+    // System under test
+    const { sut } = createSutWithProducts();
+    expect(sut.total()).toBe(41);
+    expect(sut.totalWithDicount()).toBe(41);
+  });
+
+  it('should add products and clear cart', () => {
+    // System under test
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.clear();
+    expect(sut.items.length).toBe(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should remove products', () => {
+    // System under test
+    const { sut } = createSutWithProducts();
+    sut.removeItem(1);
+    expect(sut.items.length).toBe(1);
+    sut.removeItem(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+});
